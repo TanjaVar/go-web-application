1aced40b5eae2ac636f7a7e83c7fcfe2500fce9d
Tanja Varvio <tanja.varvio@koud.fi>
2024-01-14 15:57:14 +0200

    Add  comments

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 )
 
-
+// Homepage handler
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "this is Home page")
 }
 
+// About page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValue(2, 2)
 	_, _ = fmt.Fprintf(w, "This is the about page and 2 + 2 is %d", sum)
 }
 
+// Add two integers and returns the sum
 func addValue(x, y int) int {
 	return x + y
 }
